de548dc00262ab17f8152a57f47d78ae0557e5c8
Michael <x.grizly@gmail.com>
2015-04-04 14:25:37 +0300

    file struncture created1 [app]

---
+++ QuestApp/www/js/controllers.js
@@ -0,0 +1,8 @@
+angular.module('starter.controllers', [])
+
+.controller('QuestSelectCtrl', ['$scope','$rootScope', function($scope, $rootScope) {
+
+}])
+.controller('TeamSelectCtrl', ['$scope','$rootScope', function($scope, $rootScope) {
+
+}]);
